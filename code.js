0ab56d85bfc3239b05775cc8c5ae26674b5b1a95
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-09-11 10:57:23 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
-function divideAndConquerSum(a) {
-    return a;
+//Resources:
+// https://alchemical-bytes.medium.com/how-to-use-recursion-to-divide-and-conquer-6d9a0488abc3
+// Looked at past repositories from this course, specifically divide-and-conquer-sum-ClaytonBrown4741 for help 
+//code.js
+//Maxie Machado 
+//Algorithms: Divide and Conquer Sum 
+// Plagiarism Statement: I certify that I have listed all sources used to complete this exercise, 
+// including the use of any Large Language Models. All of the work is my own, except where 
+// stated otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is
+// suspected, charges may be filed against me without prior notice.
+
+function divideAndConquerSum(a)
+{
+    if (a.length === 0)
+    {
+        return 0;
+    }
+    
+    if (a.length === 1)
+    {
+        return a[0];
+        return a;
+    }
+    
+    let third = Math.floor(a.length / 3);
+    let left = a.slice(0, third);
+    let middle = a.slice(third, 2 * third);
+    let right = a.slice(2 * third);
+    
+    return divideAndConquerSum(left) + divideAndConquerSum(middle) + divideAndConquerSum(right);
 }
+
